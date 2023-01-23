c554d05c931aa75004db0b73735a4791e5528c61
Nori <loveandfight2186@yahoo.co.jp>
2023-01-22 23:36:56 -0800

    implement watcher deep option

---
+++ watcher deep/main.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+  data: () => ({
+    colors: [
+      { name: 'Red' },
+      { name: 'Green' },
+      { name: 'Blue' },
+    ]
+  }),
+  watch: {
+    colors: {
+      handler: function (newValue, oldValue) {
+        console.log('update!')
+      },
+      deep: true
+    }
+  },
+  methods: {
+    onClick: function (event) {
+      this.colors[1].name = 'White'
+    }
+  }
+})
+
+app.mount('#app')
