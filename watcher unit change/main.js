b8dd458989c9c4797c28ac4154747df922768902
Nori <loveandfight2186@yahoo.co.jp>
2023-01-22 22:56:17 -0800

    implement watcher for unit change

---
+++ watcher unit change/main.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data: () => ({
+    km: 0,
+    m: 0
+  }),
+  watch: {
+    km: function (value) {
+      console.log(value)
+      this.km = value
+      this.m = value * 1000
+    },
+    m: function (value) {
+      this.m = value
+      this.km = value / 1000
+    }
+  }
+})
+
+app.mount('#app')
