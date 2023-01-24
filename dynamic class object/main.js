1d6ab06f21885f33c6c5964c76d175681205b5da
Nori <loveandfight2186@yahoo.co.jp>
2023-01-23 22:32:47 -0800

    use object in data option for class

---
+++ dynamic class object/main.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+  data: () => ({
+    classObject: {
+      large: true,
+      'text-danger': true
+    }
+  }),
+  watch: {
+  },
+  methods: {
+  }
+})
+
+app.mount('#app')
