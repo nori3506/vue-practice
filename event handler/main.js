078845ce9a5f188dd5dad9a422c3a9c5623e1069
Nori <loveandfight2186@yahoo.co.jp>
2023-01-29 23:43:54 -0800

    implement method, event and

--- event handler/main.js
+++ event handler/main.js
@@ -1,12 +1,23 @@
 const app = Vue.createApp({
   data: () => ({
     counter: 0,
+    message: ''
   }),
   watch: {
   },
   methods: {
     clickHandler: function() {
       this.counter++
+    },
+    clickHandler1: function(event) {
+      this.counter++
+      console.log(event.target)
+      console.log(event.target.id)
+    },
+    clickHandler2: function($event, message) {
+      console.log(message)
+      this.message = message
+      console.log($event)
     }
   }
 })
